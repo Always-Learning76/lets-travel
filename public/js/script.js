9c49869d2652acc7c4deb8c36ecba2857e7cbb4c
Jermaine Goss <jermainegoss@Jermaines-MacBook-Air.local>
2020-06-26 09:03:59 -0400

    Full Project

---
+++ public/js/script.js
@@ -0,0 +1,60 @@
+let showBtn = document.querySelector('.show-products');
+let list = document.querySelector('.product-list');
+let addForm = document.querySelector('.add-product-form');
+let UpdateForm = document.querySelector('.update-product-form');
+
+showBtn.addEventListener('click', function() {
+    list.innerHTML = '';
+    fetch('http://localhost:3000/products')
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach((product) => {
+            let li = document.createElement('li');
+            li.textContent = `${product.id} - ${product.name} - $${product.price}`;
+            list.appendChild(li);
+        })
+    })
+})
+
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    fetch('http://localhost:3000/products', { 
+    method: 'POST',
+    headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name:document.getElementById('add-product-name').value,
+            price:document.getElementById('add-product-price').value,
+        })
+    }).then((response) => response.text())
+    .then((data) => console.log(data));
+})
+
+
+
+
+
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let id = document.getElementById('update-product-id').value;
+    fetch('http://localhost:3000/products/' + id, { 
+    method: 'PUT',
+    headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            price:document.getElementById('update-product-price').value,
+        })
+    }).then((response) => response.text())
+    .then((data) => console.log(data));
+})
+
+
+
+
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+}) 
+
